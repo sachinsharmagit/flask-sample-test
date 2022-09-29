31a3ff471c26bc90f6ca909db4209bed4fef3d23
sachinsharmagit <97381214+sachinsharmagit@users.noreply.github.com>
2022-09-29 12:07:56 +0530

    Update App.java

--- src/main/java/org/intellipaat/flask_sample_test/App.java
+++ src/main/java/org/intellipaat/flask_sample_test/App.java
@@ -15,7 +15,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.setProperty("webdriver.chrome.driver","../../../chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
