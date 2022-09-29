fb22ef938bb894d4b043ae9e6e30bda95459bbe0
sachin sharma <sachin.sharma2@ihsmarkit.com>
2022-09-29 11:23:55 +0530

    testing project v1

---
+++ src/main/java/org/intellipaat/flask_sample_test/App.java
@@ -0,0 +1,28 @@
+package org.intellipaat.flask_sample_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+/**
+ * flask-sample-test
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.setProperty("webdriver.chrome.driver","../../../chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://3.80.97.59:82/");
+        WebElement element = driver.findElement(By.tagName("h1"));
+        String val = element.getText();
+        Assert.assertEquals("Test case failed", "Hello world! This message is from Kubernetes!", val);
+        driver.close();
+    }
+}
