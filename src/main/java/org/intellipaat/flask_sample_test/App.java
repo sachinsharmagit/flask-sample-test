93566a6f79c374ee8a99cdd56882bef7f4bb35e6
sachinsharmagit <97381214+sachinsharmagit@users.noreply.github.com>
2022-09-29 12:54:12 +0530

    Update App.java

--- src/main/java/org/intellipaat/flask_sample_test/App.java
+++ src/main/java/org/intellipaat/flask_sample_test/App.java
@@ -17,9 +17,7 @@ public class App
     {
         System.setProperty("webdriver.chrome.driver","chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
+        options.addArguments("--headless --disable-dev-shm-usage --no-sandbox");
         WebDriver driver = new ChromeDriver(options);
         driver.get("http://3.80.97.59:82/");
         WebElement element = driver.findElement(By.tagName("h1"));
